9f7c0d4cf0bd3300c9c56a2e4ba236a46ca585a2
Edgar <edo.jan.94@bk.ru>
2025-03-02 12:02:40 +0300

    fix

--- src/js/modules/questions.js
+++ src/js/modules/questions.js
@@ -3,7 +3,7 @@ export const questionsOpen = () => {
 
     items.forEach((item) => {
         item.addEventListener("click", (e) => {
-            if (e.target.matches(".questions__list-item") || e.target.matches(".list__item-open")) {
+            if (e.target.matches(".questions__list-item") || e.target.matches(".list__item-open") || e.target.matches(".list__item-title")) {
                 console.log(e.target);
                 item.querySelector(".list__item-content").classList.toggle("list__item-content_active");
                 item.querySelector(".list__item-open").classList.toggle("list__item-open_active");
